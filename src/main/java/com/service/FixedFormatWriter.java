6c696925e34dbfbf9b25a38a59a2dece10985fee
thisouza01 <tihsouza01@gmail.com>
2025-07-01 22:57:37 -0300

    Criação do arquivo formatado.

--- src/main/java/com/service/FixedFormatWriter.java
+++ src/main/java/com/service/FixedFormatWriter.java
@@ -1,32 +1,28 @@
 package src.main.java.com.service;
+import src.main.java.com.model.Customer;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class FixedFormatWriter {
 
-    public static void main(String[] args) throws IOException {
-        String filename = "customer.dat";
-
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            writer.write(formatRecord("ID", "Name", "Balance"));
-            writer.newLine();
-
-            writer.write(formatRecord("0001", "Thiago Souza", "1250.75"));
-            writer.newLine();
-            writer.write(formatRecord("0002", "Gabi", "500.25"));
-            writer.newLine();
-            System.out.println("File created successfully: " + filename);
+    public void writeCustomer(List<Customer> customers, String filePath) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Customer customer : customers){
+                String line = formatRecord(customer);
+                writer.write(line);
+                writer.newLine();
+            }
         } catch (IOException e) {
             System.err.println("Error writing file: " + e.getMessage());
         }
     }
 
-    private static String formatRecord(String id, String name, String balance){
-        String formattedId = String.format("%-10s", id).substring(0, 10);
-        String formattedName = String.format("%-30s", name).substring(0, 30);
-        String formattedBalance = String.format("%-9s", balance).substring(0, 9);
-
-        return formattedId + formattedName + formattedBalance;
+    private static String formatRecord(Customer customer){
+        return String.format("%-4s%-20s%010.2f",
+                customer.getId(),
+                customer.getName(),
+                customer.getBalance());
     }
 }
